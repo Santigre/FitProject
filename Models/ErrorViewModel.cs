2bb06ca05a0dbd7ae2787d3c43f0f536f005ab71
Santiago Beltran <beltran.santiago1@gmail.com>
2022-09-26 22:15:29 -0700

    Start of food nutrition
    Made a class for ingredients and their macros along with a constructor.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Fitproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/IngredientsModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fitproject.Models
+{
+    public class IngredientsModel
+    {
+        public int ID { get; set; }
+        public string IngredientName { get; set; }
+        public int Calories { get; set; }
+
+        public int Protiens { get; set; }
+        public int Carbohydrates { get; set; }
+        public int Fats { get; set; }
+
+        public IngredientsModel(int iD, string Name, int cal, int carbs, int pro, int fats)
+        {
+            ID = iD;
+            IngredientName = Name;
+            Calories = cal;
+            Carbohydrates = carbs;
+            Protiens = pro;
+            Fats = fats;
+        }
+    }
+
+    public class IngredientData
+    {
+        public List<IngredientsModel> Ingredients;
+
+        public IngredientData()
+        {
+            Ingredients = new List<IngredientsModel>();
+
+            Ingredients.Add(new IngredientsModel(1, "Chicken", 284, 53, 0, 6));
+        }
+    }
+}
